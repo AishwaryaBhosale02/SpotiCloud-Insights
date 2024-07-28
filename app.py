70d478f0a122d6ff9a5174eb9357cbf5535a0bd3
AishwaryaBhosale02 <143247162+AishwaryaBhosale02@users.noreply.github.com>
2024-07-29 01:21:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-
-# Import your function from analyze_sentiment.py
 from analyze_sentiment import analyze_sentiment_and_generate_wordclouds
 
 app = Flask(__name__)
@@ -20,10 +15,7 @@ def analyze():
     # Perform sentiment analysis and generate word clouds
     df = pd.DataFrame({'Review': [review]})
     df, wordcloud_positive_path, wordcloud_negative_path, wordcloud_neutral_path = analyze_sentiment_and_generate_wordclouds(df)
-    
-    # Determine sentiment label based on compound score
-    # Since you're no longer calculating compound scores in the function,
-    # you can determine sentiment directly from the returned dataframe
+
     sentiment = df['Sentiment'].iloc[0]
     
     return render_template('results.html', review=review, sentiment=sentiment, 
